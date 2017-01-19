baf13e586b7e6002e843180d6c6cc3db40675b36
rsimmons1 <rees.polylabs@gmail.com>
2017-01-19 17:25:27 -0500

    Update pageHandler.js

--- ideaWeb/static/ideaWeb/js/pageHandler.js
+++ ideaWeb/static/ideaWeb/js/pageHandler.js
@@ -2,12 +2,6 @@ $("#back").click(function(){
 		window.location.replace('#firstPage/1')
 })
 
-$("#search").focus(function(){
-		$("#searchBar").css({'opacity': '1','transition': 'opacity 0.5s linear'});
-	});
-$("#search").focusout(function(){
-	$("#searchBar").css({'opacity': '0.1','transition': 'opacity 0.5s linear'});
-});
 $("#search").keyup(function(event){
 	    if(event.keyCode == 13){
 	        $("#clicker").click();
